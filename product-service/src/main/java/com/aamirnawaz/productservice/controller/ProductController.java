b3ad966f800c99ab189c1c0ae97372bdf88d53b7
madisondavisnapua <madisondavis.napua@gmail.com>
2024-01-10 15:52:47 +0500

    Api gateway - spring boot api gateway done

--- product-service/src/main/java/com/aamirnawaz/productservice/controller/ProductController.java
+++ product-service/src/main/java/com/aamirnawaz/productservice/controller/ProductController.java
@@ -13,7 +13,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/product")
+@RequestMapping("/products")
 @RequiredArgsConstructor
 public class ProductController {
 
